2d713060e313ae399ab05ce42370c79cdc381bfd
Ben <boralben@gmail.com>
2020-12-05 20:25:17 -0600

    Adds the option to delegate script writing to ccl staff at the district level.

--- src/main/java/com/ccl/grandcanyon/types/District.java
+++ src/main/java/com/ccl/grandcanyon/types/District.java
@@ -25,6 +25,7 @@ public class District extends GCBase {
   public static final String LAST_STALE_SCRIPT_NOTIFICATION = "last_stale_script_notification";
   public static final String STATUS = "status";
   public static final String TIME_ZONE = "time_zone";
+  public static final String DELEGATE_SCRIPT = "delegate_script";
 
   private int districtId;
   private String state;
@@ -37,6 +38,7 @@ public class District extends GCBase {
   private Timestamp lastStaleScriptNotification;
   private Status status;
   private String timezone;
+  private Boolean delegateScript;
 
   private String info;
 
@@ -59,6 +61,7 @@ public class District extends GCBase {
     this.callTargets = new ArrayList<>();
     this.status = Status.valueOf(rs.getString(STATUS));
     this.timezone = rs.getString(TIME_ZONE);
+    this.delegateScript = rs.getBoolean(DELEGATE_SCRIPT);
 
     boolean retrieveCallTargets = false;
     ResultSetMetaData metaData = rs.getMetaData();
@@ -185,6 +188,14 @@ public class District extends GCBase {
   }
 
   public void setTimeZone(String timezone) { this.timezone = timezone; }
+
+  public Boolean getDelegateScript() {
+    return delegateScript;
+  }
+
+  public void setDelegateScript(Boolean delegateScript) {
+    this.delegateScript = delegateScript;
+  }
 
   public String readableName() {
     switch (this.getNumber()){
